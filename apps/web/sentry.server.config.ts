be9553ce36980e270c485ea06fd47574d13255cb
kelvanb97 <kelvanb97@gmail.com>
2025-12-30 11:00:07 -0800

    fix: remove sentry configs

--- apps/web/sentry.server.config.ts
+++
@@ -1,8 +0,0 @@
-import { init } from "@sentry/nextjs"
-
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-init({
-	dsn: process.env["NEXT_PUBLIC_SENTRY_DSN"]!,
-	tracesSampleRate: 1,
-	debug: false,
-})
